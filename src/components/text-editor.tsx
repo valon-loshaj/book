751dd23e8c98ca2f2243d901436f5d994b8e5eb8
valon <valonloshaj@gmail.com>
2024-03-11 17:51:08 -0400

    Add new files and implement cell-related functionality

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -6,6 +6,7 @@ const TextEditor: React.FC = () => {
 	const ref = useRef<HTMLDivElement | null>(null);
 
 	const [editing, setEditing] = useState(false);
+	const [value, setValue] = useState("# Header");
 
 	useEffect(() => {
 		const listener = (event: MouseEvent) => {
@@ -30,19 +31,26 @@ const TextEditor: React.FC = () => {
 	if (editing) {
 		return (
 			<div className='text-editor' ref={ref}>
-				<MDEditor />
+				<MDEditor
+					value={value}
+					onChange={(v) => {
+						setValue(v || "");
+					}}
+				/>
 			</div>
 		);
 	}
 
 	return (
 		<div
-			className='text-editor'
+			className='text-editor card'
 			onClick={() => {
 				setEditing(true);
 			}}
 		>
-			<MDEditor.Markdown source={"# Header"} />
+			<div className='card-content'>
+				<MDEditor.Markdown source={value} />
+			</div>
 		</div>
 	);
 };
