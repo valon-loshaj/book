4ba96d22523fbc1b3acc94f5df34cda6125f5c00
valon <valonloshaj@gmail.com>
2024-03-19 16:16:24 -0400

    Add code-cell.css and import it in code-cell.tsx

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import "./code-cell.css";
 import { useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
@@ -13,21 +14,36 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 	const { updateCell, createBundle } = useActions();
 	const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+	const cumulativeCode = useTypedSelector((state) => {
+		const { data, order } = state.cells;
+		const orderedCells = order.map((id) => data[id]);
+
+		const cumulativeCode = [];
+		for (let c of orderedCells) {
+			if (c.type === "code") {
+				cumulativeCode.push(c.content);
+			}
+			if (c.id === cell.id) {
+				break;
+			}
+		}
+		return cumulativeCode;
+	});
 
 	useEffect(() => {
 		if (!bundle) {
-			createBundle(cell.id, cell.content);
+			createBundle(cell.id, cumulativeCode.join("\n"));
 			return;
 		}
 		const timer = setTimeout(async () => {
-			createBundle(cell.id, cell.content);
+			createBundle(cell.id, cumulativeCode.join("\n"));
 		}, 750);
 
 		return () => {
 			clearTimeout(timer);
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [cell.id, cell.content, createBundle]);
+	}, [cell.id, cumulativeCode.join("\n"), createBundle]);
 
 	return (
 		<Resizable direction='vertical'>
@@ -45,7 +61,20 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 						theme='dark'
 					/>
 				</Resizable>
-				{bundle && <Preview code={bundle.code} err={bundle.err} />}
+				<div className='progress-wrapper'>
+					{!bundle || bundle.loading ? (
+						<div className='progress-cover'>
+							<progress
+								className='progress is-small is-primary'
+								max='100'
+							>
+								Loading
+							</progress>
+						</div>
+					) : (
+						<Preview code={bundle.code} err={bundle.err} />
+					)}
+				</div>
 			</div>
 		</Resizable>
 	);
